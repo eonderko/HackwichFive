1017474ec4c77aec4e86cc1a4fdf72cea52fc6ed
eonderko <cmstudent@uhwo-acm-07.local>
2020-02-20 10:36:17 -1000

    completed part 8

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+
+    var currentIndex = 0
+    var favoriteFoodArray = ["Pizza", "Buffalo Wings", "Scrambled Eggs", "Fried Chicken", "McGriddle"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.firstLabel.text = "My Favorite Foods"
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any)
+    
+    {
+        self.secondLabel.text = favoriteFoodArray[currentIndex]
+        if self.currentIndex < self.favoriteFoodArray.count
+        {
+            currentIndex+=1
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)
+        }
+        else {
+            
+            (sender as! UIButton).isEnabled = false
+            
+            
+        }
+    }
+  
 }
 
